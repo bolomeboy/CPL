91e5cb022b208d23539df0766e403b1793a7bcdb
bolomeboy <bolobeats@outlook.com>
2026-08-20 14:58:08 -0700

    Update tabs.js

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -10,136 +10,48 @@ export const tabs = [
 
     {
         icon: 'emoji_events',
-        label: 'California Primeira Liga',
+        label: 'CPL',
         dest: '/cpl',
-        nest: true,
         key: 'cpl',
-        children: [
-            {
-                icon: 'home',
-                label: 'CPL Home',
-                dest: '/cpl',
-            },
-            {
-                icon: 'leaderboard',
-                label: 'Standings',
-                dest: '/cpl/standings',
-            },
-            {
-                icon: 'sports',
-                label: 'Matchups',
-                dest: '/cpl/matchups',
-            },
-            {
-                icon: 'storage',
-                label: 'Rosters',
-                dest: '/cpl/rosters',
-            },
-            {
-                icon: 'swap_horiz',
-                label: 'Transactions',
-                dest: '/cpl/transactions',
-            },
-            {
-                icon: 'emoji_events',
-                label: 'Champions',
-                dest: '/cpl/champions',
-            },
-            {
-                icon: 'military_tech',
-                label: 'Records',
-                dest: '/cpl/records',
-            },
-        ],
     },
 
     {
         icon: 'emoji_events',
-        label: 'Segunda Liga',
+        label: 'Segunda',
         dest: '/segunda',
-        nest: true,
         key: 'segunda',
-        children: [
-            {
-                icon: 'home',
-                label: 'Segunda Home',
-                dest: '/segunda',
-            },
-            {
-                icon: 'leaderboard',
-                label: 'Standings',
-                dest: '/segunda/standings',
-            },
-            {
-                icon: 'sports',
-                label: 'Matchups',
-                dest: '/segunda/matchups',
-            },
-            {
-                icon: 'storage',
-                label: 'Rosters',
-                dest: '/segunda/rosters',
-            },
-            {
-                icon: 'swap_horiz',
-                label: 'Transactions',
-                dest: '/segunda/transactions',
-            },
-            {
-                icon: 'emoji_events',
-                label: 'Champions',
-                dest: '/segunda/champions',
-            },
-            {
-                icon: 'military_tech',
-                label: 'Records',
-                dest: '/segunda/records',
-            },
-        ],
-    },
-
-    {
-        icon: 'sync_alt',
-        label: 'Promotion & Relegation',
-        dest: '/promotion-relegation',
-        key: 'promotion',
-    },
-
-    {
-        icon: 'history',
-        label: 'League History',
-        nest: true,
-        key: 'history',
-        children: [
-            {
-                icon: 'emoji_events',
-                label: 'Champions',
-                dest: '/history/champions',
-            },
-            {
-                icon: 'leaderboard',
-                label: 'All-Time Records',
-                dest: '/history/records',
-            },
-            {
-                icon: 'history_edu',
-                label: 'Season History',
-                dest: '/history/seasons',
-            },
-        ],
     },
 
     {
         icon: 'menu_book',
-        label: 'League Rules',
-        dest: '/rules',
-        key: 'rules',
+        label: 'League Info',
+        nest: true,
+        key: 'league_info',
+        children: [
+            {
+                icon: 'sync_alt',
+                label: 'Promotion & Relegation',
+                dest: '/promotion-relegation',
+            },
+            {
+                icon: 'menu_book',
+                label: 'League Rules',
+                dest: '/rules',
+            },
+        ],
     },
 
     {
-        icon: 'sports_football',
-        label: 'Go to Sleeper',
-        dest: `https://sleeper.app/leagues/${leagueID}`,
-        key: 'sleeper',
+        icon: 'more_horiz',
+        label: 'More',
+        nest: true,
+        key: 'more',
+        children: [
+            {
+                icon: 'sports_football',
+                label: 'Go to Sleeper',
+                dest: `https://sleeper.app/leagues/${leagueID}`,
+            },
+        ],
     },
 ];
