94f68ff8b6ab30fc069788a933ae5d98e5c93065
bolomeboy <bolobeats@outlook.com>
2026-08-20 15:56:31 -0700

    Update index.svelte

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -76,7 +76,7 @@
 </style>
 
 <nav>
-	<a href="/"><img id="logo" alt="league logo" src="/badge.png" /></a>
+	<a href="/"><img id="logo" alt="California Primeira Liga logo" src="/cpl-logo.png" /></a>
 
 	<div class="container">
 		<IconButton
