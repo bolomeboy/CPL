a9ea02ab48a4e8eb9abba44091a961216b95ff53
bolomeboy <bolobeats@outlook.com>
2026-08-26 12:38:36 -0700

    Update +page.svelte

--- src/routes/manager/+page.svelte
+++ src/routes/manager/+page.svelte
@@ -1,23 +1,40 @@
 <script>
-	import LinearProgress from '@smui/linear-progress';
-    import {Manager} from '$lib/components';
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
+    import LinearProgress from '@smui/linear-progress';
+    import { Manager } from '$lib/components';
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
 
-	export let data;
-	const {managers, manager, managersInfo} = data;
+    export let data;
+
+    const {
+        managers,
+        manager,
+        managerID,
+        rosterID,
+        year,
+        division,
+        managersInfo
+    } = data;
 
     onMount(() => {
-        if(!managers.length) goto('/');
-        if(manager < 0) goto("/managers");
-    })
+        if (!managers.length) {
+            goto('/');
+        }
+
+        // Only redirect if we have neither an old manager
+        // index nor a new Sleeper manager ID.
+        if (manager < 0 && !managerID) {
+            goto('/managers');
+        }
+    });
 </script>
 
 <style>
-	.main {
-		position: relative;
-		z-index: 1;
-	}
+    .main {
+        position: relative;
+        z-index: 1;
+    }
+
     .loading {
         display: block;
         width: 85%;
@@ -27,18 +44,39 @@
 </style>
 
 <div class="main">
-        {#await managersInfo}
-            <!-- promise is pending -->
-            <div class="loading">
-                <p>Retrieving managers...</p>
-                <LinearProgress indeterminate />
-            </div>
-        {:then [rostersData, leagueTeamManagers, leagueData, transactionsData, awards, records]}
-            {#if managers.length && manager > -1}
-                <Manager {awards} {records} {manager} {managers} {rostersData} {leagueTeamManagers} rosterPositions={leagueData.roster_positions} {transactionsData} />
-            {/if}
-        {:catch error}
-            <!-- promise was rejected -->
-            <p>Something went wrong: {error.message}</p>
-        {/await}
+
+    {#await managersInfo}
+
+        <div class="loading">
+            <p>Retrieving managers...</p>
+            <LinearProgress indeterminate />
+        </div>
+
+    {:then [rostersData, leagueTeamManagers, leagueData, transactionsData, awards, records]}
+
+        {#if managers.length && (manager > -1 || managerID)}
+
+            <Manager
+                {awards}
+                {records}
+                {manager}
+                {managers}
+                {managerID}
+                {rosterID}
+                {year}
+                {division}
+                {rostersData}
+                {leagueTeamManagers}
+                rosterPositions={leagueData.roster_positions}
+                {transactionsData}
+            />
+
+        {/if}
+
+    {:catch error}
+
+        <p>Something went wrong: {error.message}</p>
+
+    {/await}
+
 </div>
