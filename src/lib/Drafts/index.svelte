b2ce3e762db3983efeccb412fcc07067e7971337
bolomeboy <bolobeats@outlook.com>
2026-08-25 17:31:01 -0700

    Update index.svelte

--- src/lib/Drafts/index.svelte
+++ src/lib/Drafts/index.svelte
@@ -3,10 +3,13 @@
     import LinearProgress from '@smui/linear-progress';
     import Draft from './Draft.svelte';
 
-    export let cplDraftsData;
-    export let cplPreviousDraftsData;
+    export let upcomingDraftData;
+    export let previousDraftsData;
     export let leagueTeamManagersData;
     export let playersData;
+
+    export let leagueName = '';
+    export let logo = '';
 </script>
 
 <style>
@@ -21,158 +24,125 @@
         text-align: center;
     }
 
-    h5 {
-        text-align: center;
-        font-size: 1.15em;
-        margin-top: 30px;
-        margin-bottom: 10px;
-    }
-
     h6 {
         text-align: center;
     }
 
-    .leagueSection {
-        margin-bottom: 40px;
+    .leagueHeader {
+        text-align: center;
+        margin-top: 30px;
+        margin-bottom: 20px;
     }
 
-    .redTitle {
-        color: #b00000;
+    .leagueLogo {
+        width: 90px;
+        height: 90px;
+        object-fit: contain;
+        margin-bottom: 8px;
     }
 
-    .greenTitle {
-        color: #008c45;
+    .leagueHeader h3 {
+        margin: 0;
+    }
+
+    .divider {
+        margin: 40px 0;
     }
 </style>
 
 
-{#await waitForAll(cplDraftsData, leagueTeamManagersData, playersData)}
+{#await waitForAll(
+    upcomingDraftData,
+    leagueTeamManagersData,
+    playersData
+)}
+
     <div class="loading">
-        <p>Retrieving CPL Red and CPL Green drafts...</p>
+
+        <p>
+            Retrieving {leagueName} draft...
+        </p>
+
         <br />
+
         <LinearProgress indeterminate />
+
     </div>
 
-{:then [{ redDraft, greenDraft }, leagueTeamManagers, { players }]}
+{:then [upcomingDraft, leagueTeamManagers, {players}]}
 
-    <h4>📝 2026 Inaugural Drafts</h4>
+    <div class="leagueHeader">
 
-
-    <!-- CPL RED -->
-
-    <div class="leagueSection">
-
-        <h5 class="redTitle">
-            🔴 CPL Red
-        </h5>
-
-        {#if redDraft}
-            <Draft
-                draftData={redDraft}
-                leagueTeamManagers={leagueTeamManagers}
-                year={redDraft.year}
-                {players}
+        {#if logo}
+            <img
+                class="leagueLogo"
+                src={logo}
+                alt="{leagueName} logo"
             />
-        {:else}
-            <p style="text-align: center;">
-                CPL Red draft information is not available yet.
-            </p>
         {/if}
+
+        <h3>{leagueName}</h3>
 
     </div>
 
+    <h4>
+        {upcomingDraft.year} Draft
+    </h4>
 
-    <hr />
-
-
-    <!-- CPL GREEN -->
-
-    <div class="leagueSection">
-
-        <h5 class="greenTitle">
-            🟢 CPL Green
-        </h5>
-
-        {#if greenDraft}
-            <Draft
-                draftData={greenDraft}
-                leagueTeamManagers={leagueTeamManagers}
-                year={greenDraft.year}
-                {players}
-            />
-        {:else}
-            <p style="text-align: center;">
-                CPL Green draft information is not available yet.
-            </p>
-        {/if}
-
-    </div>
-
-
-    <!-- PREVIOUS DRAFTS -->
-
-    {#await waitForAll(cplPreviousDraftsData, leagueTeamManagersData, playersData)}
-        <hr />
-
-        <h4>Previous Drafts</h4>
-
-        <div class="loading">
-            <p>Retrieving previous drafts...</p>
-            <br />
-            <LinearProgress indeterminate />
-        </div>
-
-    {:then [{ redDrafts, greenDrafts }, leagueTeamManagers, { players }]}
-
-        {#if redDrafts.length || greenDrafts.length}
-
-            <hr />
-
-            <h4>Previous Drafts</h4>
-
-            {#if redDrafts.length}
-                <h5 class="redTitle">🔴 CPL Red</h5>
-
-                {#each redDrafts as previousDraft}
-                    <h6>{previousDraft.year} Draft</h6>
-
-                    <Draft
-                        draftData={previousDraft}
-                        previous={true}
-                        {leagueTeamManagers}
-                        year={previousDraft.year}
-                        {players}
-                    />
-                {/each}
-            {/if}
-
-
-            {#if greenDrafts.length}
-                <h5 class="greenTitle">🟢 CPL Green</h5>
-
-                {#each greenDrafts as previousDraft}
-                    <h6>{previousDraft.year} Draft</h6>
-
-                    <Draft
-                        draftData={previousDraft}
-                        previous={true}
-                        {leagueTeamManagers}
-                        year={previousDraft.year}
-                        {players}
-                    />
-                {/each}
-            {/if}
-
-        {/if}
-
-    {:catch error}
-
-        <p>Something went wrong: {error.message}</p>
-
-    {/await}
+    <Draft
+        draftData={upcomingDraft}
+        {leagueTeamManagers}
+        year={upcomingDraft.year}
+        {players}
+    />
 
 {:catch error}
 
-    <p>Something went wrong: {error.message}</p>
+    <p>
+        Something went wrong: {error.message}
+    </p>
+
+{/await}
+
+
+{#await waitForAll(
+    previousDraftsData,
+    leagueTeamManagersData,
+    playersData
+)}
+
+{:then [previousDrafts, leagueTeamManagers, {players}]}
+
+    {#if previousDrafts.length}
+
+        <hr class="divider" />
+
+        <h4>
+            {leagueName} Previous Drafts
+        </h4>
+
+        {#each previousDrafts as previousDraft}
+
+            <h6>
+                {previousDraft.year} Draft
+            </h6>
+
+            <Draft
+                draftData={previousDraft}
+                previous={true}
+                {leagueTeamManagers}
+                year={previousDraft.year}
+                {players}
+            />
+
+        {/each}
+
+    {/if}
+
+{:catch error}
+
+    <p>
+        Something went wrong: {error.message}
+    </p>
 
 {/await}
